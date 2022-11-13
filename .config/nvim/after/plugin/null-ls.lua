225214d854ff1ab0d176f3c25bc242457ef51c8d
Jimvid <victor@jimvid>
2022-11-13 22:03:43 +0100

    Add LSPkind, reorganize and update lsp config and autocompletion, switch vsnip for luasnip

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -28,7 +28,7 @@ null_ls.setup({
 	end,
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.diagnostics.alex,
--- .config/nvim/lua/jimvid/base.lua
+++ .config/nvim/lua/jimvid/base.lua
@@ -7,17 +7,18 @@ vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.wrap = false -- No Wrap lines
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
+	pattern = "*",
+	command = "set nopaste",
 })
