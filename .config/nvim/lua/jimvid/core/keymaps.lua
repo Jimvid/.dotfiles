e96cff818bba6393eae98d2e24f54fe4baa7494e
Jimvid <victor@jimvid>
2025-05-15 09:02:53 +0200

    remove :w like keybinds

--- .config/nvim/lua/jimvid/core/keymaps.lua
+++ .config/nvim/lua/jimvid/core/keymaps.lua
@@ -1,10 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>w", ":w<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>wq", ":wq<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>q", ":q<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>cq", ":cq<cr>", { noremap = true })
-
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 
