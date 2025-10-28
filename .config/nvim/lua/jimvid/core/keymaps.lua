039589d780a6d2e688e7d8bc88965a3b038da06b
Jimvid <victor@jimvid.com>
2025-10-28 16:22:49 +0100

    add neotest with jest adapter

--- .config/nvim/lua/jimvid/core/keymaps.lua
+++ .config/nvim/lua/jimvid/core/keymaps.lua
@@ -28,3 +28,20 @@ vim.keymap.set("n", "<leader>NN", ":NoNeckPain<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessions.sh<CR>", { silent = true })
+
+-- Neotest
+vim.keymap.set("n", "<leader>n", "", { desc = "🧪 Test" })
+vim.keymap.set("n", "<leader>nr", "<cmd>lua require('neotest').run.run()<cr>", { desc = "Run nearest test" })
+vim.keymap.set("n", "<leader>nf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+    { desc = "Run current file" })
+vim.keymap.set("n", "<leader>na", "<cmd>lua require('neotest').run.run({ suite = true })<cr>", { desc = "Run all tests" })
+vim.keymap.set("n", "<leader>nd", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+    { desc = "Debug nearest test" })
+vim.keymap.set("n", "<leader>ns", "<cmd>lua require('neotest').run.stop()<cr>", { desc = "Stop test" })
+vim.keymap.set("n", "<leader>nn", "<cmd>lua require('neotest').run.attach()<cr>", { desc = "Attach to nearest test" })
+vim.keymap.set("n", "<leader>no", "<cmd>lua require('neotest').output.open()<cr>", { desc = "Show test output" })
+vim.keymap.set("n", "<leader>np", "<cmd>lua require('neotest').output_panel.toggle()<cr>",
+    { desc = "Toggle output panel" })
+vim.keymap.set("n", "<leader>nv", "<cmd>lua require('neotest').summary.toggle()<cr>", { desc = "Toggle summary" })
+vim.keymap.set("n", "<leader>nc", "<cmd>lua require('neotest').run.run({ suite = true, env = { CI = true } })<cr>",
+    { desc = "Run all tests with CI" })
---
+++ .config/nvim/lua/jimvid/plugins/neotest.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-jest",
+        "nvim-neotest/neotest-go",
+    },
+    config = function()
+        local status_ok, neotest = pcall(require, "neotest")
+        if not status_ok then
+            return
+        end
+
+        local jest = require("neotest-jest")
+
+        neotest.setup({
+            summary = {
+                open = "botright vsplit | vertical resize 80",
+            },
+            adapters = {
+                jest({
+                    jestCommand = "yarn test --",
+                    cwd = function(path)
+                        return vim.fn.getcwd()
+                    end,
+                }),
+            },
+        })
+    end,
+}
