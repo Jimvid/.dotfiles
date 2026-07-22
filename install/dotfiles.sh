d0aa8330b52e33e9e393faed6fbc301b341630f2
Jimvid <victor@jimvid.com>
2026-07-22 10:55:14 +0200

    add setup scripts

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir ~/dev
+mkdir ~/dev/work
+mkdir ~/dev/personal
+
 cd $HOME/.dotfiles
 stow . --adopt
 git reset --hard
---
+++ install/flatpak.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sudo apt install flatpak
+
+# Install Slack
+flatpak install flathub com.slack.Slack
+
+# Install Plexamp
+flatpak install flathub com.plexamp.Plexamp
